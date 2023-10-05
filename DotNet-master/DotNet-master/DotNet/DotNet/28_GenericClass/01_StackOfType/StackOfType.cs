1d198c20c2a7d61af9c7ee25469baa591962cebc
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-10-05 20:24:35 +0900

    연습

--- DotNet-master/DotNet-master/DotNet/DotNet/28_GenericClass/01_StackOfType/StackOfType.cs
+++ DotNet-master/DotNet-master/DotNet/DotNet/28_GenericClass/01_StackOfType/StackOfType.cs
@@ -7,7 +7,6 @@ class StackOfType
     {
         Stack<string> stack = new Stack<string>();
         stack.Push("First"); // Push() 메서드는 문자열만 입력 가능
-        stack.Push("Second");
         Console.WriteLine(stack.Pop()); // 문자열로 바로 출력
     }
 }
--- DotNet-master/DotNet-master/DotNet/DotNet/28_GenericClass/05_EnumerableDemo/EnumerableDemo.cs
+++ DotNet-master/DotNet-master/DotNet/DotNet/28_GenericClass/05_EnumerableDemo/EnumerableDemo.cs
@@ -8,13 +8,13 @@ class EnumerableDemo
         // 1부터 5까지
         var numbers = Enumerable.Range(1, 5);
         foreach (var n in numbers)
-            Console.Write("{0}\t", n);
+            Console.Write("{0}", n);
         Console.WriteLine();
 
         // -1을 5개
         var sameNumbers = Enumerable.Repeat(-1, 5);
         foreach (var n in sameNumbers)
-            Console.Write("{0}\t", n);
+            Console.Write("{0}", n);
         Console.WriteLine();
     }
 }
